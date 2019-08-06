29d405e52a235d6a10481fe01f0aee1022c4c67b
kimserey lam <kimserey.lam@jet.com>
2019-08-06 08:56:08 +0100

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HelloWorldComponent } from './hello-world.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: HelloWorldComponent,
+  data: { title: 'Hello World' }
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
